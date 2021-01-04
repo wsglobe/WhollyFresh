b0ff05c440529803739244bf4fb4082f38707de2
Walter Wang <wsglobe@gmail.com>
2021-01-03 21:18:54 -0600

    start app.js

---
+++ whollyfresh/src/App.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navbar from "./Navbar.js";
+import Home from "./Home.js";
+import About from "./About.js";
+import Products from "./Products.js";
+import ProductDetails from "./ProductDetails.js";
+import Cart from "./Cart.js";
