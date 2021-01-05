29e411ead080227a4c5d18a8a3eee38f30844671
Walter Wang <wsglobe@gmail.com>
2021-01-04 19:13:23 -0600

    change nav-brand name

--- whollyfresh/src/Navbar.js
+++ whollyfresh/src/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar(props) {
   return (
     <nav className="navbar">
       <NavLink to="/" className="nav-brand">
-        SuperM
+        whollyFresh
       </NavLink>
       <ul>
         <li className="nav-item">
